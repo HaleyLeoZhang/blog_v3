d1ebad3303965f6265dc2eda3d9c9436c05d8dd5
云天河 <229270575@qq.com>
2018-10-10 13:26:50 +0800

    Patch: 友情链接 添加功能丢失

--- public/static_pc/admin/js_v3.0/friend_link.js
+++ public/static_pc/admin/js_v3.0/friend_link.js
@@ -37,16 +37,67 @@
             confirm_ajax(init);
         });
     };
-    
+    // @action：创建链接
+    FriendLink.prototype.friend_link_create = function () {
+        var _this = this;
+        var _template = `
+<div class="row" style="width: 420px;  margin-left:7px; margin-top:10px;">
+    <div class="col-sm-12">
+        <div class="input-group"> <span class="input-group-addon">名称</span>
+            <input id="title_0" type="password" class="form-control " placeholder="请输入站点名"> </div>
+    </div>
+    <div class="col-sm-12" style="margin-top: 10px">
+        <div class="input-group"> <span class="input-group-addon">链接</span>
+            <input id="href_0" type="text" class="form-control" placeholder="请输入站点链接"> </div>
+    </div>
+    <div class="col-sm-12" style="margin-top: 10px">
+        <div class="input-group"> <span class="input-group-addon">权重&nbsp;</span>
+            <input id="weight_0" type="number" class="form-control" placeholder="请输入数字"> </div>
+    </div>
+    <div class="col-sm-12" style="margin-top: 10px;margin-bottom: 10px">
+        <button class="btn btn-info" style="width: 100%;"
+         onclick="friend_link.friend_link_create_request()">提交</button>
+    </div>
+</div>
+        `;
+        $(".friend_link_create").on("click", function () {
+            //页面层-自定义
+            layer.open({
+                type: 1,
+                title: false,
+                closeBtn: 0,
+                shadeClose: true,
+                area:['450px', 'auto'],
+                skin: 'layui-layer-lan',
+                anim: 4, //动画类型
+                content: _template,
+            });
+        });
+    };
+
     // @action：发起创建请求
     FriendLink.prototype.friend_link_create_request = function () {
-        var init = {
-            "api_url": admin_api('common', 'friend_link_update'),
-            "data": {
+        var _data = {
                 "title": $("#title_0").val(),
                 "href": $("#href_0").val(),
                 "weight": $("#weight_0").val(),
-            },
+        };
+        // - 数据校验
+        if(  _data.title == ''){
+            layer.msg('站点名不能为空');
+            return ;
+        }
+        if(  _data.href == ''){
+            layer.msg('链接不能为空');
+            return ;
+        }
+        if(  _data.weight == ''){
+            layer.msg('权重不能为空');
+            return ;
+        }
+        var init = {
+            "api_url": admin_api('common', 'friend_link_update'),
+            "data": _data,
             "res_text": "创建成功",
             "need_confirm": false,
         };
