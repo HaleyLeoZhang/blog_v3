65e243e981c03a958088c44e5af7246a32db6e2a
云天河 <229270575@qq.com>
2018-10-11 13:32:30 +0800

    修改文章内容页的Keyword

--- resources/views/module/article/index.blade.php
+++ resources/views/module/article/index.blade.php
@@ -3,7 +3,7 @@
 {{-- 页面标题 --}}
 @section('set_title') {{ $article_obj->title }}_云天河博客 @endsection
 {{-- 页面SEO --}}
-@section('seo_keywords')云天河博客,{{ $article_obj->title }}云天河博客@endsection
+@section('seo_keywords'){{ $article_obj->title }},云天河博客@endsection
 {{-- 页面描述 --}}
 @section('seo_description') 这篇文章主要阐述了{{ $article_obj->descript }}的相关内容 @endsection
 {{-- 文章详情 --}}
