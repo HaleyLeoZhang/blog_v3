aa083366b206ef7f572773bbaf84c48165de75a0
云天河 <229270575@qq.com>
2018-11-22 12:03:12 +0800

    + 访客足迹分析，解析头部数据，等待操作一次性脚本，与后续批量解析脚本

---
+++ app/Models/Logs/VisitorFootMarkAnalysis.php
@@ -0,0 +1,30 @@
+<?php
+namespace App\Models\Logs;
+
+use Illuminate\Database\Eloquent\Model;
+
+// 用户访问足迹分析
+
+class VisitorFootMarkAnalysis extends Model
+{
+    protected $table      = 'visitor_footer_mark_analysis';
+    protected $connection = 'yth_blog_ext';
+
+    protected $fillable = [
+        'ip', // 访客IP
+        'device_type', // 设备类型：-2->没有相关信息、-1->其他、0->蜘蛛、1->移动端、2->PC
+        'device_name', // 设备详细名称
+        'referer', // 来源站点
+        'target', // 访问地址
+        'created_at', // 创建时间（与访客足迹的采集时间保持一致）
+    ];
+
+    /**
+     * 设备类型
+     */
+    const DEVICE_TYPE_UNKNOW = -2;
+    const DEVICE_TYPE_OTHERS = -1;
+    const DEVICE_TYPE_SPIDER = 0;
+    const DEVICE_TYPE_MOBILE = 1;
+    const DEVICE_TYPE_PC = 2;
+}
--- app/Repositories/Log/Logic/VisitorLogLogic.php
+++ app/Repositories/Log/Logic/VisitorLogLogic.php
@@ -44,4 +44,52 @@ class VisitorLogLogic
         VisitorLookLog::create($log);
     }
 
+    /**
+     * 访客足迹拆解
+     * @param App\Models\Logs\VisitorFooterMark $foot_mark
+     * @return void
+     */
+    public static function visitor_foot_analysis($foot_mark)
+    {
+        $analysis = new VisitorFootMarkAnalysis();
+        $header   = json_decode(preg_replace('/(\[|\])/', '', $foot_mark->header));
+        // 获取访客IP
+        $analysis->ip = $foot_mark->ip;
+        // 设备类型：-2->没有相关信息、-1->其他、0->蜘蛛、1->移动端、2->PC
+        $agent = 'user-agent';
+        switch (true) {
+            case preg_match('/phone|pad|pod|iPhone|iPod|ios|iPad|Android|Mobile|BlackBerry|IEMobile|MQQBrowser|JUC|Fennec|wOSBrowser|BrowserNG|WebOS|Symbian|Windows Phone/', $header->$agent, $match):
+                $device_type = VisitorFootMarkAnalysis::DEVICE_TYPE_MOBILE;
+                $device_name = $match[0];
+                break;
+            case preg_match('/Safari|MSIE|Firefox|Chrome|Maxthon|MetaSr|360SE/', $header->$agent, $match):
+                $device_type = VisitorFootMarkAnalysis::DEVICE_TYPE_PC;
+                $device_name = $match[0];
+                break;
+            case preg_match('/Baiduspider|360spider|Googlebot|bingbot/', $header->$agent, $match):
+                $device_type = VisitorFootMarkAnalysis::DEVICE_TYPE_SPIDER;
+                $device_name = $match[0];
+                break;
+            case '' != $header->$agent:
+                $device_type = VisitorFootMarkAnalysis::DEVICE_TYPE_UNKNOW;
+                $device_name = '';
+                break;
+            default:
+                $device_type = VisitorFootMarkAnalysis::DEVICE_TYPE_OTHERS;
+                $device_name = '';
+                break;
+        }
+
+        $analysis->device_type = $device_type;
+        // 设备详细名称
+        $analysis->device_name = $device_name;
+        // 来源站点
+        $analysis->referer = $header->referer ?? '';
+        // 访问地址
+        $analysis->target = $foot_mark->url;
+        // 创建时间（与访客足迹的采集时间保持一致）
+        $analysis->created_at = $foot_mark->created_at;
+        $analysis->save();
+    }
+
 }
