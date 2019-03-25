261c503d48c7217326e078823511e78a7353b2a5
云天河 <hlzblog@vip.qq.com>
2019-03-25 14:30:48 +0800

    Debug

--- resources/views/module/wechat/tpl_pic_content.blade.php
+++ resources/views/module/wechat/tpl_pic_content.blade.php
@@ -1,17 +1,17 @@
 <xml>
-    <ToUserName><![CDATA[{!!$ToUserName!!}]]></ToUserName>
-    <FromUserName><![CDATA[{!!$FromUserName!!}]]></FromUserName>
-    <CreateTime>{!!$CreateTime!!}</CreateTime>
-    <MsgType><![CDATA[news]]></MsgType>
-    <ArticleCount>{!! count($news) !!}</ArticleCount>
-    <Articles>
+  <ToUserName><![CDATA[{!!$ToUserName!!}]]></ToUserName>
+  <FromUserName><![CDATA[!!$FromUserName!!}]]></FromUserName>
+  <CreateTime>{!!$CreateTime!!}</CreateTime>
+  <MsgType><![CDATA[news]]></MsgType>
+  <ArticleCount>{!! count($news) !!}</ArticleCount>
+  <Articles>
     @foreach ($news as $index => $one)
-        <item>
-            <Title><![CDATA[{!!$one['Title']!!}]]></Title>
-            <Description><![CDATA[{!!$one['Description']!!}]]></Description>
-            <PicUrl><![CDATA[{!!$one['PicUrl']!!}]]></PicUrl>
-            <Url><![CDATA[{!!$one['Url']!!}]]></Url>
-        </item>
-    @endforeach
-    </Articles>
+    <item>
+      <Title><![CDATA[{!!$one['Title']!!}]]></Title>
+      <Description><![CDATA[{!!$one['Description']!!}]]></Description>
+      <PicUrl><![CDATA[{!!$one['PicUrl']!!}]]></PicUrl>
+      <Url><![CDATA[{!!$one['Url']!!}]]></Url>
+    </item>
+  @endforeach
+  </Articles>
 </xml>
