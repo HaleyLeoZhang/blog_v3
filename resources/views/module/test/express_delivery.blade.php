b6bafa7d807611dd51e9cec3153ab2acbf881e20
云天河 <hlzblog@vip.qq.com>
2020-03-09 23:46:29 +0800

    增加快递查询页的头部信息

--- resources/views/module/test/express_delivery.blade.php
+++ resources/views/module/test/express_delivery.blade.php
@@ -4,7 +4,9 @@
     <head>
         <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
         <title>物流信息查询</title>
-        
+        <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+        <meta name="viewport" content="width=device-width,initial-scale=1">
+        <link rel="shortcut icon" href="//tencent.cdn.hlzblog.top/favicon.ico"  />
         <link rel="stylesheet" href="/static_pc/css/express_delivery.css">
     </head>
 
