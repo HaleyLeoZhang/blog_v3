825e673b0c710d65c19b8fddc8c5e31c1c885bea
lex <hlzblog@vip.qq.com>
2024-08-19 18:17:45 +0800

    update

--- app/Bussiness/Common/Logic/CommonLogic.php
+++ app/Bussiness/Common/Logic/CommonLogic.php
@@ -15,10 +15,12 @@ class CommonLogic
      */
     public static function memorabilia_bg()
     {
-        $bg_url = CacheLogic::get_memorabilia_cache();
-        if (!$bg_url) {
-            $bg_url = CacheLogic::ini_memorabilia_cache();
-        }
+        // 2024-8-19 因为6年过去，网络带宽不再是问题，直接开放源文件
+        $bg_url = "/Others/memorabilia/music/memorabilia_bg.mp3";
+//        $bg_url = CacheLogic::get_memorabilia_cache();
+//        if (!$bg_url) {
+//            $bg_url = CacheLogic::ini_memorabilia_cache();
+//        }
         return $bg_url;
     }
 
